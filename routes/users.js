2097e0b7b94a863b81c71b782a8be6f07d8bcda5
charlie50503 <cate50503@gmail.com>
2022-05-28 15:49:38 +0900

    添加其他路由

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const users = require('../controllers/user')
+const {isAuth} = require('../middleware/auth');
+const { handleErrorAsync } = require('../utils/errorHandle')
 /* GET users listing. */
-router.get('/', users.get);
+router.get('/',isAuth,handleErrorAsync(users.get));
+router.get('/profile',isAuth, handleErrorAsync(users.getProfile));
+router.patch('/profile',isAuth,  handleErrorAsync(users.patchProfile));
+router.post('/sign_up', handleErrorAsync(users.postSignUp));
+router.post('/sign_in', handleErrorAsync(users.postSignIn));
+router.post('/updatePassword',isAuth, handleErrorAsync(users.postUpdatePassword));
 
 module.exports = router;
