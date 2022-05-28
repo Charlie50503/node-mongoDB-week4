d9c84e7309d791310c7e58bb4f25a5629f59b673
charlie50503 <cate50503@gmail.com>
2022-05-28 15:48:47 +0900

    添加msg

---
+++ msg/msg.js
@@ -0,0 +1,10 @@
+const msg = {
+  fieldNotCorrect:'欄位未填寫正確！',
+  passwordNotMatch:'密碼不一致！',
+  passwordLengthNotMatch:'密碼字數低於 8 碼',
+  emailFormatNotCorrect:'Email 格式不正確',
+  notLoginIn:'你尚未登入!',
+  passwordNotCorrect:'密碼不正確!',
+}
+
+module.exports = msg
