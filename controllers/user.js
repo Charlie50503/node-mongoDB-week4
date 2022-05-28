64fe3121212284e6885abaa6671d0acb1e540282
charlie50503 <cate50503@gmail.com>
2022-05-28 15:49:55 +0900

    添加formatter

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,10 @@
 const User = require('../models/user')
 const successHandle = require('../services/successHandle')
 const appError = require('../utils/appError')
-const validator = require('validator');
-const msg = require('../msg/msg');
-const bcrypt = require('bcryptjs/dist/bcrypt');
-const {generateSendJWT} = require('../middleware/auth');
+const validator = require('validator')
+const msg = require('../msg/msg')
+const bcrypt = require('bcryptjs/dist/bcrypt')
+const { generateSendJWT } = require('../middleware/auth')
 const users = {
   async get(req, res) {
     const userData = await User.find()
@@ -18,57 +18,57 @@ const users = {
     const userData = await User.find()
     successHandle(req, res, userData)
   },
-  async postSignUp(req, res,next) {
+  async postSignUp(req, res, next) {
     const { email, password, confirmPassword, name } = req.body
-    
+
     if (!email || !password || !confirmPassword || !name) {
       return next(appError('400', msg.fieldNotCorrect, next))
     }
-    if(password!==confirmPassword){
+    if (password !== confirmPassword) {
       return next(appError('400', msg.fieldNotCorrect, next))
     }
-    if(!validator.isLength(password,{min:8})){
+    if (!validator.isLength(password, { min: 8 })) {
       return next(appError('400', msg.passwordLengthNotMatch, next))
     }
-    if(!validator.isEmail(email)){
-      return next(appError('400',msg.emailFormatNotCorrect,next))
+    if (!validator.isEmail(email)) {
+      return next(appError('400', msg.emailFormatNotCorrect, next))
     }
 
-    const bcryptPassword = await bcrypt.hash(req.body.password,12)
+    const bcryptPassword = await bcrypt.hash(req.body.password, 12)
     try {
       const newUser = await User.create({
         email,
-        password:bcryptPassword,
-        name
+        password: bcryptPassword,
+        name,
       })
     } catch (error) {
       if (error.code === 11000) {
-        return next(appError('400','已註冊此用戶',next))
+        return next(appError('400', '已註冊此用戶', next))
       }
-      return next(appError('400', '不明原因錯誤',next))
+      return next(appError('400', '不明原因錯誤', next))
     }
-    generateSendJWT(newUser,201,res)
+    generateSendJWT(newUser, 201, res)
   },
-  async postSignIn(req, res,next) {
+  async postSignIn(req, res, next) {
     const { email, password } = req.body
-    
+
     if (!email || !password) {
       return next(appError('400', msg.fieldNotCorrect, next))
     }
-    if(!validator.isLength(password,{min:8})){
+    if (!validator.isLength(password, { min: 8 })) {
       return next(appError('400', msg.passwordLengthNotMatch, next))
     }
-    if(!validator.isEmail(email)){
-      return next(appError('400',msg.emailFormatNotCorrect,next))
+    if (!validator.isEmail(email)) {
+      return next(appError('400', msg.emailFormatNotCorrect, next))
     }
-    const user = await User.findOne({email}).select('+password')
-    console.log("user",user);
-    const auth = await bcrypt.compare(password,user.password)
-    console.log("auth",auth);
-    if(!auth){
-      return next(appError(400,msg.passwordNotCorrect,next))
+    const user = await User.findOne({ email }).select('+password')
+    console.log('user', user)
+    const auth = await bcrypt.compare(password, user.password)
+    console.log('auth', auth)
+    if (!auth) {
+      return next(appError(400, msg.passwordNotCorrect, next))
     }
-    generateSendJWT(newUser,201,res)
+    generateSendJWT(newUser, 201, res)
   },
   async postUpdatePassword(req, res) {
     const userData = await User.find()
