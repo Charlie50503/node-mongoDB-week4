3e6d3480b648df81baba0fa04af75dbed24d972f
charlie50503 <cate50503@gmail.com>
2022-05-31 22:43:09 +0900

    為token處理添加參數

--- utils/token.js
+++ utils/token.js
@@ -3,30 +3,29 @@ const appError = require('../utils/appError')
 const { handleErrorAsync } = require('../utils/errorHandle')
 const msg = require('../msg/msg')
 
-const getTokenId = handleErrorAsync(async () =>{
-  let token
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    token = req.headers.authorization.replace('Bearer ', '')
-  }
-
-  if (!token) {
-    return next(appError(401, msg.notLoginIn, next))
-  }
-
-  const decoded = await new Promise((resolve, reject) => {
-    jwt.verify(token, process.env.JWT_SECRET, (error, payload) => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve(payload)
-      }
+const getTokenId = async (req,res,next) => {
+    let token
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer')
+    ) {
+      token = req.headers.authorization.replace('Bearer ', '')
+    }
+  
+    if (!token) {
+      return next(appError(401, msg.notLoginIn, next))
+    }
+  
+    const decoded = await new Promise((resolve, reject) => {
+      jwt.verify(token, process.env.JWT_SECRET, (error, payload) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(payload)
+        }
+      })
     })
-  })
-
-  return decoded.id
-})
+    return decoded.id
+} 
 
 module.exports = { getTokenId }
