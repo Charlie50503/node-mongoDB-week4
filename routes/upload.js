e9605258553c4b5771ed192b54ae90bc9eea2d10
charlie50503 <cate50503@gmail.com>
2022-07-03 16:57:45 +0900

    fix : 上傳圖片時，如果未通過 auth 驗證 (如沒有帶 token )，會回傳 Application Error，原因是 Express 來不及解析圖片就出錯了

--- routes/upload.js
+++ routes/upload.js
@@ -5,8 +5,9 @@ const uploadImg = require('../controllers/upload')
 const {isAuth} = require('../middleware/auth');
 const {uploadOneImg} = require('../middleware/image');
 
-router.post('/',isAuth,
-handleErrorAsync(uploadOneImg)
-,handleErrorAsync(uploadImg.image))
+router.post('/',
+handleErrorAsync(uploadOneImg),
+isAuth,
+handleErrorAsync(uploadImg.image))
 
 module.exports = router;
